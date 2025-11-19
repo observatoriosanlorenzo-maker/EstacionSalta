a598a677e6f8021b075a7f5767269e29cdb7a268
observatoriosanlorenzo-maker <observatoriosanlorenzo@gmail.com>
2025-11-19 13:06:54 -0300

    Update WindyEmbed.js

--- pages/pages/localidad/omponents/WindyEmbed.js
+++ pages/pages/localidad/omponents/WindyEmbed.js
@@ -1,1 +1,15 @@
+export default function WindyEmbed({ lat, lon, zoom }) {
+  const windyUrl =
+    process.env.NEXT_PUBLIC_WINDY_EMBED_URL ||
+    `https://embed.windy.com/embed2.html?lat=${lat}&lon=${lon}&zoom=${zoom}&level=surface&overlay=wind`;
+
+  return (
+    <iframe
+      width="100%"
+      height="450"
+      frameBorder="0"
+      src={windyUrl}
+    />
+  );
+}
 
